d399ea2ec1731eaa5b24a55297e45ad8c7bf18e7
seo-jae-eun <wodms5522@naver.com>
2024-08-19 18:03:52 +0900

    frontenbackend¨테이너 ì 생성

--- src/main/java/org/example/day0902/TestController.java
+++ src/main/java/org/example/day0902/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping(method = RequestMethod.GET, value = "/test")
     public String test() {
-        return "backend 2.0.6";
+        return "backend 2.0.0";
     }
 }
