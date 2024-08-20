2d65add8614525942313ab719f31a84c77bddcb8
seo-jae-eun <wodms5522@naver.com>
2024-08-20 11:37:07 +0900

    backend version upgrade

--- src/main/java/org/example/day0902/TestController.java
+++ src/main/java/org/example/day0902/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping(method = RequestMethod.GET, value = "/test")
     public String test() {
-        return "v1";
+        return "v2";
     }
 }
